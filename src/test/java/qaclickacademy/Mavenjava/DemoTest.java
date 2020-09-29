f078b163e22854e12cac2a63f69e50a16f993923
AbhiSAbhi <Abhinew91@gmail.com>
2020-09-29 19:58:28 +0530

    final commit by EST

---
+++ src/test/java/qaclickacademy/Mavenjava/DemoTest.java
@@ -0,0 +1,12 @@
+package qaclickacademy.Mavenjava;
+
+import org.junit.Test;
+
+public class DemoTest 
+{
+    @Test
+    public void latest()
+    {
+    	System.out.println("Newly added TC");
+    }
+}
